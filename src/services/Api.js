4eedc818de152a053df02330abdd86674e261839
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-07 07:46:09 +0300

    Added requests to fetch weather and forecast

---
+++ src/services/Api.js
@@ -0,0 +1,13 @@
+import axios from '../plugins/axios';
+
+export default class Api {
+  static searchWeather = async (location) => {
+    const response = await axios.get(`/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`);
+    return response;
+  };
+
+  static searchForecast = async (location) => {
+    const response = await axios.get(`/forecast?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`);
+    return response;
+  };
+}
