f546c64a9ae8b2ac09e8f7e2a6029d02e1928331
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-10 16:50:06 +0300

    Return metric units

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,12 +2,12 @@ import axios from '../plugins/axios';
 
 export default class Api {
   static searchWeather = async (location) => {
-    const response = await axios.get(`/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`);
+    const response = await axios.get(`/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`);
     return response;
   };
 
   static searchForecast = async (location) => {
-    const response = await axios.get(`/forecast?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`);
+    const response = await axios.get(`/forecast?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`);
     return response;
   };
 }
